4e1deb2cc7aa6853d92c6efb5c19192a7f2afeb7
IluaRoot <142495530+IluaRoot@users.noreply.github.com>
2024-01-06 13:53:31 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,11 +11,25 @@ class reload_nginx {
     enable => true,
   }
 }
+class firewall {
+}
 
 node 'slave1.puppet' {
-  include install_nginx, reload_nginx
+  include install_nginx
+  -> file { 'Copy conf file slave1':
+       ensure => file,
+       source => 'puppet:///conf.f/static.conf',
+       path => "/etc/nginx/conf.d/",
+  }
+  -> include reload_nginx
 }
 
 node 'slave2.puppet' {
-  include install_nginx, reload_nginx
+  include install_nginx
+  -> file { 'Copy conf file slave2':
+       ensure => file,
+       source => 'puppet:///conf.f/dynamic.conf',
+       path => "/etc/nginx/conf.d/",
+  }
+  -> include reload_nginx
 }
