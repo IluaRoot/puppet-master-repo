9d80c6a60e5e114f103da0618385ca10209433ce
IluaRoot <142495530+IluaRoot@users.noreply.github.com>
2024-01-06 16:29:08 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,4 +44,31 @@ node 'slave2.puppet' {
     name => 'php-fpm',
     provider => 'yum',
     }
+      -> file { '/vagrant/conf.f/dynamic.conf':
+       ensure => 'present',
+       source => '/vagrant/conf.f/dynamic.conf',
+       path => "/etc/nginx/conf.d/dynamic.conf",
+  }
+    -> file { ['/var/www', '/var/www/dynamic']:
+       ensure => 'directory',
+       owner => 'nginx',
+       group => 'nginx',
+       mode => '0755',
+    }
+    
+    -> file { '/vagrant/index.php':
+       ensure => 'present',
+       source => '/vagrant/index.php',
+       path => "/var/www/dynamic/index.php",
+    }
+    -> file { '/etc/nginx/nginx.conf':
+       ensure => present,
+       }
+    exec { 'Disable default site': 
+       command => "/bin/sed -i \'s/listen 80 default_server;/#listen 80 default_server;/\' /etc/nginx/nginx.conf"
+    }
+    service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
 }
